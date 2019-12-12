ffe570f9f81c25c3285edf7e7820606012cfaaa5
Ken <ken@kenh.fr>
2019-12-12 20:30:36 +0100

    put option simu if env

--- pytilt-embedded/pytilt.py
+++ pytilt-embedded/pytilt.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 import time
-
-## for real device
-# from classes.raspi_classes import BubblerRaspi as Bubbler
-# from classes.raspi_classes import TiltRaspi as Tilt
-# from classes.raspi_classes import init
-
-## for simulation
-from classes.sim_classes import BubblerSim as Bubbler
-from classes.sim_classes import TiltSim as Tilt
-from classes.sim_classes import init
-
+import os
+        
+SIM = os.environ.get('PYTILT_SIMULATE', None)
+if SIM == "FALSE":
+    print("Using real device..")
+    from classes.raspi_classes import BubblerRaspi as Bubbler
+    from classes.raspi_classes import TiltRaspi as Tilt
+    from classes.raspi_classes import init
+else:
+    print("Using simulation..")
+    from classes.sim_classes import BubblerSim as Bubbler
+    from classes.sim_classes import TiltSim as Tilt
+    from classes.sim_classes import init
 
 def monitor():
     bubbler = Bubbler(14)
